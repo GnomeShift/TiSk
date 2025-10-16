a0146249bd1bdcb17828aba796e42ae34155194b
GnomeShift <151649479+GnomeShift@users.noreply.github.com>
2025-10-16 23:53:21 +0300

    refactor(back): update validation error messages

--- src/main/java/com/gnomeshift/tisk/ticket/CreateTicketDTO.java
+++ src/main/java/com/gnomeshift/tisk/ticket/CreateTicketDTO.java
@@ -11,18 +11,18 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class CreateTicketDTO {
-    @NotBlank(message = "Title can't be empty")
-    @Size(max = 255, message = "Title length can't exceed 255 characters")
+    @NotBlank(message = "Title required")
+    @Size(min = 1, max = 255, message = "Title must be between 1 and 255 characters")
     private String title;
 
     @Size(max = 5000, message = "Description can't exceed 5000 characters")
     private String description;
 
-    @NotNull(message = "Status can't be null")
+    @NotNull(message = "Status required")
     @Enumerated(EnumType.STRING)
     private TicketStatus status = TicketStatus.OPEN;
 
-    @NotNull(message = "Priority can't be null")
+    @NotNull(message = "Priority required")
     @Enumerated(EnumType.STRING)
     private TicketPriority priority;
 }
