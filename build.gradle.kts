5c3547c9b4d28ec018a8482b07e325e3d3e5d47f
GnomeShift <151649479+GnomeShift@users.noreply.github.com>
2026-01-05 14:20:25 +0300

    feat(back): add metrics

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("io.micrometer:micrometer-registry-prometheus:1.16.1")
 	implementation("org.mapstruct:mapstruct:1.6.3")
 	implementation("io.jsonwebtoken:jjwt-api:0.13.0")
 	implementation("io.github.cdimascio:dotenv-java:3.2.0")
